58ad886b013f79cd432dff5a0d0e3cdd3863ccd0
Abe <abedormancy@gmail.com>
2015-09-25 11:37:33 +0800

    delete someLine then add some line

--- src/net/gicp/abe/test/TestStream.java
+++ src/net/gicp/abe/test/TestStream.java
@@ -2,7 +2,11 @@ package net.gicp.abe.test;
 
 public class TestStream {
 	public static void main(String[] args) {
-		System.out.println("hello world");
 		System.out.println("hello git");
+		
+		System.out.println("all add then modify!");
+		
+		
+		System.out.println("and then ... modifyed");
 	}
 }
