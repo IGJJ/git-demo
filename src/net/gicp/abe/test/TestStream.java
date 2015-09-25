d8dcdb366831b7a95761b345e9a19ffa66d1eb8b
Abe <abedormancy@gmail.com>
2015-09-25 11:45:52 +0800

    跳过add:)

--- src/net/gicp/abe/test/TestStream.java
+++ src/net/gicp/abe/test/TestStream.java
@@ -3,10 +3,9 @@ package net.gicp.abe.test;
 public class TestStream {
 	public static void main(String[] args) {
 		System.out.println("hello git");
-		
 		System.out.println("all add then modify!");
-		
-		
 		System.out.println("and then ... modifyed");
+		
+		//这是跳过暂存区域的版本。
 	}
 }
