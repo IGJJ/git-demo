e09bdb699fd256ed7ca9f99e6da7d6296da021bd
Abe <abedormancy@gmail.com>
2015-09-25 12:13:13 +0800

    删除了全部信息
    just a demo
    只是测试
    不要激动。

--- src/net/gicp/abe/test/TestStream.java
+++ src/net/gicp/abe/test/TestStream.java
@@ -2,11 +2,6 @@ package net.gicp.abe.test;
 
 public class TestStream {
 	public static void main(String[] args) {
-		System.out.println("hello git");
-		System.out.println("all add then modify!");
-		System.out.println("and then ... modifyed");
-		
-		//这是跳过暂存区域的版本。
-		//gui
+		System.out.println("是了.");
 	}
 }
