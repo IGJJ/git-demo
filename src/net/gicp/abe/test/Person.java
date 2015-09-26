da81e21598ea00c8b4d907cc8335e6ae65e4cf99
= <=>
2015-09-26 11:48:43 +0800

    嗯哼。

--- src/net/gicp/abe/test/Person.java
+++ src/net/gicp/abe/test/Person.java
@@ -1,7 +1,7 @@
-pakcage net.gicp.abe.test;
+﻿pakcage net.gicp.abe.test;
 
 public class Person {
 	public String getInfo() {
-		System.out.println("hello world");
+		System.out.println("你好世界");
 	}
 }
